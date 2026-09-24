cdfad5b0094541a2ec4e6712b37e28c74bf6c079
somkenemuscle <somkeneoj@gmail.com>
2026-09-24 22:22:40 +0100

    feat: integrate API endpoints

--- server/dist/lib/parseIdParam.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseIdParam = parseIdParam;
-function parseIdParam(value) {
-    const id = Number(value);
-    return Number.isInteger(id) ? id : null;
-}
--- server/dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const app_1 = require("./app");
-const PORT = process.env.PORT ?? 4000;
-app_1.app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
-});
