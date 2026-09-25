db61ef65af327de7918b054ca8f8223d05cd744e
somkenemuscle <somkeneoj@gmail.com>
2026-09-25 12:25:45 +0100

    chore: use render provided port

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,5 +4,5 @@ import { app } from './app'
 const PORT = process.env.PORT ?? 4000
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`)
+  console.log(`Server listening on port ${PORT}`)
 })
